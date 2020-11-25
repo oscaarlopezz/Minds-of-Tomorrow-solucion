0efc4da2c2dc6d3f68f77e97856fcd11b1844100
oscaarlopezz <oscarlm27@gmail.com>
2020-11-25 13:48:40 +0100

    archivo java

---
+++ js/codigo.js
@@ -0,0 +1,15 @@
+    // When the user scrolls down 50px from the top of the document, resize the header's font size
+    window.onscroll = function() {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.getElementById("nav").style.backgroundColor = "rgba(255,255,255,0.5)";
+            document.getElementById("nav").style.backdropFilter = "blur(4px)";
+        } else {
+            document.getElementById("nav").style.backgroundColor = "rgba(0,0,0,0.0)";
+            document.getElementById("nav").style.backdropFilter = "blur(0px)";
+
+        }
+    }
